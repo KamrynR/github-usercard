9eb795a0886b4716b58279d75ee2036a430e6ab7
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-06-11 20:35:58 -0500

    pr

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -30,6 +30,7 @@
 
 const followersArray = [];
 
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
